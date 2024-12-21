8111f3c8f3477649703c3ad88535dd26418b5636
rikikangsc2-eng <nueapi11@gmail.com>
2024-12-21 23:51:45 +0000

    Running konfigurasi

--- rikiRun.bash
+++ rikiRun.bash
@@ -1,13 +1,17 @@
-#!/bin/bash
+start_server() {
+  while true; do
+    echo -e "HTTP/1.1 200 OK\r\nContent-Length: $(echo -n "Bot running on koyeb✓" | wc -c)\r\n\r\nBot running on koyeb✓" | nc -l -p 3000 -q 1
+  done
+}
 
-# Infinite loop to restart the script if it crashes
-while true; do
-  echo "Starting node index.js..."
-  
-  # Jalankan node index.js
-  node index.js
-  
-  # Jika crash, tampilkan pesan dan tunggu 1 menit sebelum restart
-  echo "The script crashed. Restarting in 1 minute..."
-  sleep 60
-done
+start_node_app() {
+  while true; do
+    echo "Starting node index.js..."
+    node index.js
+    echo "index.js crashed! Restarting in 1 minute..."
+    sleep 60
+  done
+}
+
+start_server &  
+start_node_app  
